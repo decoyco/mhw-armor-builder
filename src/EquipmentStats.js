da29df3e3dd7446ad649b356b904e6716dd60e76
Josh Coyco <jac6308@gmail.com>
2020-04-22 11:05:12 -0700

    Added defense, attack, affinity, and element stats to equipment stat calc

--- src/EquipmentStats.js
+++ src/EquipmentStats.js
@@ -13,23 +13,45 @@ export default function EquipmentStats(props) {
     } = props
     const {
         t_attack,
+        t_element,
+        t_elementValue,
+        t_hidden,
         t_defense,
         t_skills,
         t_affinity
     } = GetEquipmentStats(head,chest,gloves,waist,boots,weapon,charm)
 
-    const [attack, setAttack] = useState('0')
-    const [defense, setDefense] = useState('0')
-    const [affinity, setAffinity] = useState('0')
+    const [attack, setAttack] = useState(0)
+    const [element, setElement] = useState('')
+    const [elementValue, setElementValue] = useState('0')
+    const [hidden, setHidden] = useState(false)
+    const [defense, setDefense] = useState(0)
+    const [affinity, setAffinity] = useState(0)
     const [skills, setSkills] = useState('')
 
     useEffect(() => {
         setDefense(t_defense)
-    }, [t_defense])
+        setAttack(t_attack)
+        setElement(t_element)
+        setElementValue(t_elementValue)
+        setHidden(t_hidden)
+        setAffinity(t_affinity)
+    }, [t_defense, t_attack, t_affinity, t_element, t_elementValue, t_hidden])
 
     return (
-        <div>
-            {defense}
-        </div>
+        <>
+            <div>
+                Defense: {defense}
+            </div>
+            <div>
+                Attack: {attack}
+            </div>
+            <div>
+                {(element !== '' && elementValue !== 0) && element.charAt(0).toUpperCase() + element.slice(1) + ' :' + elementValue + (hidden ? "(Hidden)" : '')}
+            </div>
+            <div>
+                Affinity: {affinity}
+            </div>
+        </>
     )
 }
--- src/GetEquipmentStats.js
+++ src/GetEquipmentStats.js
@@ -5,18 +5,62 @@ export default function GetEquipmentStats(head,chest,gloves,waist,boots,weapon,c
     const [t_attack, setTAttack] = useState(0)
     const [t_defense, setTDefense] = useState(0)
     const [t_affinity, setTAffinity] = useState(0)
+    const [t_elementValue, setTElementValue] = useState(0)
+    const [t_element, setTElement] = useState('')
+    const [t_hidden, setTHidden] = useState(false)
     const [t_skills, setTSkills] = useState('')
 
     useEffect(() => {
-        let temp = 0
+        let temp_defense = 0
+        let temp_attack = 0
+        let temp_affinity = 0
+        let temp_elementValue = 0
+        let temp_hidden = false
+        let temp_element = ''
         if(head !== '')
         {
-            temp += head.defense.base
+            temp_defense += head.defense.base
         }
-        setTDefense(temp)
+        if(chest !== '')
+        {
+            temp_defense += chest.defense.base
+        }
+        if(gloves !== '')
+        {
+            temp_defense += gloves.defense.base
+        }
+        if(waist !== '')
+        {
+            temp_defense += waist.defense.base
+        } 
+        if(boots !== '')
+        {
+            temp_defense += boots.defense.base
+        }
+        if(weapon !== '')
+        {
+            temp_attack += weapon.attack.display
+            if(weapon.attributes.affinity)
+                temp_affinity += weapon.attributes.affinity
+            if(weapon.elements[0])
+            {
+                temp_element = weapon.elements[0].type
+                temp_elementValue = weapon.elements[0].damage
+                temp_hidden = weapon.elements[0].hidden
+            }
+        }
+        setTDefense(temp_defense)
+        setTAttack(temp_attack)
+        setTElement(temp_element)
+        setTElementValue(temp_elementValue)
+        setTHidden(temp_hidden)
+        setTAffinity(temp_affinity)
     }, [head,chest,gloves,waist,boots,weapon,charm])
     return {
         t_attack,
+        t_element,
+        t_elementValue,
+        t_hidden,
         t_defense,
         t_affinity,
         t_skills
